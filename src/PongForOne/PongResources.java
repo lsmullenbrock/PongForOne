8b5efabb0dd3cdab3852e13f63abcaf5b6240292
Lee Mullenbrock <lee.mullenbrock@gmail.com>
2017-02-17 14:54:17 -0600

    Rename stuff

--- src/PongForOne/PongResources.java
+++ src/PongForOne/PongResources.java
@@ -5,5 +5,5 @@ import javafx.scene.image.Image;
 interface PongResources {
     Image PLAYER_IMAGE = new Image("file:images/player.png");
     Image BALL_IMAGE = new Image("file:images/ball.png");
-    String GAME_OVER_TEXT = "You lose.\nHit Enter to play again.\nHit ESCAPE to Quit.";
+    String GAME_OVER_TEXT = "You lose.\nHit ENTER to play again.\nHit ESCAPE to Quit.";
 }
